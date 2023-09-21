1d5f4a059f9bce2972fb4eef41f717215ac3bc62
AnnaHort <annahort3d@gmail.com>
2023-09-21 22:45:30 +0300

    update store (delete localStore save)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,11 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactSlice";
-import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// Об'єднуємо всі редуктори які ми хочемо зберегти
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-});
-
-export const persistor = persistStore(store);
+  reducer:{
+    contacts: contactsReducer,
+  }
+})
 
 
 
